0329b307a026cc4fc2ad597d1b04e56b0b87f56d
Jorge <jorge@tecnoydes.com>
2020-02-14 17:47:13 +0100

    problema con los marcadores

--- src/es/MapComponent/MapComponent.jsx
+++ src/es/MapComponent/MapComponent.jsx
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import API_KEY from '../../key';
 
-//test
-const contacts = [
-  { name: 'Pepe', lat: 41.529616, lng: 2.434130 },
-  { name: 'Pepa', lat: 41.528103, lng: 2.433834 },
-  { name: 'Pepi', lat: 41.530192, lng: 2.422994 }
+
+if ("geolocation" in navigator) {
+  /* la geolocalización está disponible */
+  return window.navigator.geolocation.getCurrentPosition(function getCords(pos) {
+    let lat = pos.coords.latitude;
+    let lng = pos.coords.longitude;
+    let mypos = {
+      lat: lat,
+      lng: lng
+    }
+  })
+} else {
+  /* la geolocalización NO está disponible */
+  alert('Tu dispositivo no soporta la geolocalización.');
+}
+
+
+console.log(getCords());
+var contacts = [
+  { name: 'Yo', lat: '', lng: '' },
+  { name: '·', lat: 41.529616, lng: 2.434130 },
+  { name: '·', lat: 41.528103, lng: 2.433834 },
+  { name: '·', lat: 41.530192, lng: 2.422994 }
 ];
-const mylat = window.navigator.geolocation.getCurrentPosition((pos) => pos.coords.latitude);
-const mylng = window.navigator.geolocation.getCurrentPosition((pos) => pos.coords.longitude);
+
 const MarkersC = ({ text }) => <div className="contact">{text}</div>;
 
 export default class MapComponent extends Component {
@@ -29,18 +46,19 @@ export default class MapComponent extends Component {
       // Important! Always set the container height explicitly
       <div className="mt-5" style={{ height: '80vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: API_KEY }}
-          defaultCenter={this.state.center}
+          bootstrapURLKeys={{ key: API_KEY, v: '3.31' }}
+          center={this.state.center}
           defaultZoom={this.state.zoom}
-
+          //   onChildMouseEnter={this.onChildMouseEnter}
+          //   onChildMouseLeave={this.onChildMouseLeave}
+          yesIWantToUseGoogleMapApiInternals
+        // onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
         >
-          <MarkersC lat={mylat} lng={mylng} text={'Yo'} />
           {
             contacts.map((contact, i) =>
               <MarkersC lat={contact.lat} lng={contact.lng} text={contact.name} key={i} />
             )
           }
-
         </GoogleMapReact>
       </div>
     );
